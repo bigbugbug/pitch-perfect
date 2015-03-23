c40f6e2006a54a25f82dae7f301736291dc2d4bb
bigbugbug <chichungchan@gmail.com>
2015-03-23 09:17:20 -0700

    Committed by Udacity

---
+++ PitchPerfect/PitchPerfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  PitchPerfect
+//
+//  Created by Nicole Chan on 3/7/15.
+//  Copyright (c) 2015 John Chan. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
